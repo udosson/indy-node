0150466ad6e75a66b4eefa5d3819675ac088881e
udosson <r.klemens@yahoo.de>
2021-07-09 13:27:00 +0200

    reverted change of indy-node package version

--- setup.py
+++ setup.py
@@ -27,10 +27,9 @@ with open(metadata['__file__'], 'r') as f:
 
 BASE_DIR = os.path.join(os.path.expanduser("~"), ".indy")
 
-# tests_require = ['attrs>=20.3.0', 'pytest>=6.2.2', 'pytest-xdist>=2.2.1', 'pytest-forked>=1.3.0',
-#                  'python3-indy==1.13.0-dev-1420', 'pytest-asyncio>=0.14.0']
 tests_require = ['attrs>=20.3.0', 'pytest>=6.2.2', 'pytest-xdist>=2.2.1', 'pytest-forked>=1.3.0',
-                 'python3-indy==1.13.0-dev0', 'pytest-asyncio>=0.14.0']
+                 'python3-indy==1.13.0-dev-1420', 'pytest-asyncio>=0.14.0']
+
 
 
 setup(
